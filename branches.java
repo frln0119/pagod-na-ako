1ddd72fdfb2fce4790e50fe88d1ebc813bd4786c
UnknownDev <froilanaquino1@gmail.com>
2022-06-25 01:47:35 +0800

    template

--- branches.java
+++ branches.java
@@ -4,4 +4,19 @@ import java.awt.*;
 
 public class branches extends JFrame {
 	
+  public void branchesInterface() {
+    JLabel title = new JLabel("Available Branches");
+    title.setBounds(0, 0, 0, 0);
+    JLabel fistLocation = new JLabel("CALOOCAN CITY BRANCH");
+    firstLocation.setBounds(0, 0, 0, 0);
+    add(title);
+    add(firstLocation);
+    setVisible(true);
+    setSize(800, 800);
+  }
+
+  public static void main(String[] args) {
+    branches bc = new branches();
+    bc.branchesInterface();
+  }
 }
